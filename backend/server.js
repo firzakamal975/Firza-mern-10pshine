0a29c0d25c01be4f226c54ea262629112b9ee1ad
firzakamal975 <firzakamal0@gmail.com>
2026-01-05 16:05:45 +0500

    feat: backend structure, server.js and environment setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route for testing
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
